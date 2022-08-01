925c13499bda589807ddea6aa46fd3ee55b230cd
Ekechukwu Osu <ekechukwu.osu@oneacrefund.org>
2022-08-01 17:55:17 +0100

    make swagger available for production

--- IconCargoPriceCalculator/Program.cs
+++ IconCargoPriceCalculator/Program.cs
@@ -66,11 +66,11 @@ builder.Services.AddAuthentication(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.UseHttpsRedirection();
